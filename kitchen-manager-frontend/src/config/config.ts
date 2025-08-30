c554d5551272028b93e5e6862d34a68a5bb5b4c1
SN257 <sidzt186@gmail.com>
2025-08-30 13:49:00 +0530

    Initial Commit

--- kitchen-manager-frontend/src/config/config.ts
+++ kitchen-manager-frontend/src/config/config.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 
-export const BASE_URL = import.meta.env.API_BASE_URL || 'http://localhost:3000';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export const useApiBaseUrl = () => {
   const organizationSlug = window.location.pathname.split('/')[1];
